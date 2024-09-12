c060c5fc78768bca52f9823825617ae8da33d71e
HiraYuki <g22908hs@gm.tsuda.ac.jp>
2024-09-12 19:36:30 +0900

    merge main blanch (after changed event)

--- Frontend/chicken-app/src/EventRegister.js
+++ Frontend/chicken-app/src/EventRegister.js
@@ -1,59 +1,65 @@
 import './eventRegisterStyle.css';
-import React from "react";
-import { useEffect, useState } from 'react';
-import axios from 'axios'; // axiosをインポート
-
-import ResultCard from './resultcard';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const EventRegister = () => {
+  const [eventName, setEventName] = useState('');
+  const [time, setTime] = useState('');
+  const [whereToMeet, setWhereToMeet] = useState('');
+  const [foodPreference, setFoodPreference] = useState('');
+  const [userId, setUserId] = useState('');
+  const [type, setType] = useState('');
 
-    const [eventName, setEventName] = useState('');
-    const [time, setTime] = useState('');
-    const [whereToMeet, setWhereToMeet] = useState('');
-    const [foodPreference, setFoodPreference] = useState('');
-    const [userId, setUserId] = useState('');
-    const [type, setType] = useState('');
+  const url = "http://localhost:8000/event/entry"; // URLを修正
 
-    const url = " ";
+  const PostData = (event) => {
+    event.preventDefault(); // フォームのデフォルトの送信動作を防止
 
-  const PostData = () => {
-		axios.post(url,{
-            event_name: eventName,
-            time: time,
-            where_to_meet: whereToMeet,
-            food_preference: foodPreference,
-            user_id: userId,
-            type: type
-
-        }).then((res) => {
-			console.log(res);
-		}).catch((err)=>{
-            console.log(err);
-        });
-	};
+    axios.post(url, {
+      event_name: eventName,
+      time: time,
+      where_to_meet: whereToMeet,
+      food_preference: foodPreference,
+      user_id: userId,
+      type: type
+    },{
+        headers: {
+            'Content-Type': 'application/json'  // JSON形式でデータを送信するためのヘッダー
+          }
+    }
+    )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <div>
-    <p className='title'>Event</p>
-
-    <form className="EventRegistForm" onSubmit={PostData}>
+      <p className='title'>Event</p>
+      <form className="EventRegistForm" onSubmit={PostData}>
         <label>eventname</label>
-        <input value={eventName} onChange={(cevent)=>setEventName(cevent.target.value)}/>
-        
+        <input value={eventName} onChange={(cevent) => setEventName(cevent.target.value)} />
+
         <label>time</label>
-        <input value={time} onChange={(cevent)=>setTime(cevent.target.value)}/>
-        
+        <input value={time} onChange={(cevent) => setTime(cevent.target.value)} />
+
         <label>whereToMeet</label>
-        <input value={whereToMeet} onChange={(cevent)=>setWhereToMeet(cevent.target.value)}/>
-        
+        <input value={whereToMeet} onChange={(cevent) => setWhereToMeet(cevent.target.value)} />
+
         <label>foodPreference</label>
-        <input value={foodPreference} onChange={(cevent)=>setFoodPreference(cevent.target.value)}/>
+        <input value={foodPreference} onChange={(cevent) => setFoodPreference(cevent.target.value)} />
 
         <label>type</label>
-        <input value={type} onChange={(cevent)=>setType(cevent.target.value)}/>
-        
+        <input value={type} onChange={(cevent) => setType(cevent.target.value)} />
+
+        <label>user</label>
+        <input value={userId} onChange={(cevent) => setUserId(cevent.target.value)} />
+
         <input type="submit" value="Submit" />
-    </form>
+      </form>
     </div>
   );
 };
