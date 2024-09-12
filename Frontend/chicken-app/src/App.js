ab1668ab3e45b0630953df1bd12b070c20421c3e
Atsushi Tsujimura <a.tnba1386@gmail.com>
2024-09-12 20:57:47 +0900

    プロフィール登録⇒postリクエスト⇒user_idに応じたページの表示まで

--- Frontend/chicken-app/src/App.js
+++ Frontend/chicken-app/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import EventDetail from './components/EventDetail';
 import Home from './components/Home';
 import RegisterProfile from './components/InputProfile2';
+import Profile from './components/Profile';
 import YourProfile  from './components/YourProfile';
 import { BrowserRouter as Router, Route, Link, Routes, useNavigate  } from 'react-router-dom';
 
@@ -11,7 +13,10 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/register_profile" element={<RegisterProfile />} />
+                    {/* :user_id は動的なパラメータ */}
+                     <Route path="/profile/:user_id" element={<Profile />} />
                     <Route path="/your_profile" element={<YourProfile />} />
+                    <Route path="/event_detail" element={<EventDetail />} />
 
                     {/* <Route path="/profile" element={<LunchCompanion />} />
                     <Route path="/Eventregist" element={<EventRegister />} /> */}
